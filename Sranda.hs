28121d3e3cc04b329440366d2d824b3cf820581d
Logotrop <keramdobes@gmail.com>
2019-10-03 00:49:26 +0200

    test prvocisla

--- Sranda.hs
+++ Sranda.hs
@@ -4,14 +4,20 @@ next a = a+1
 before :: Integer -> Integer
 before a = a-1
 
-squareRoot :: Integer -> Integer
-squareRoot 0 = 0
-squareRoot 1 = 1
-squareRoot n =
-   let twopows = iterate (^!2) 2
-       (lowerRoot, lowerN) =
-          last $ takeWhile ((n>=) . snd) $ zip (1:twopows) twopows
-       newtonStep x = div (x + div n x) 2
-       iters = iterate newtonStep (squareRoot (div n lowerN) * lowerRoot)
-       isRoot r  =  r^!2 <= n && n < (r+1)^!2
-  in  head $ dropWhile (not . isRoot) iters
+pow :: Integer -> Integer -> Integer
+pow a 1 = a
+pow a b = a * pow a (b-1)
+
+toInt :: Float -> Integer
+toInt a = round a
+
+testprime :: Integer -> Integer -> Bool
+testprime a 2 = not (mod a 2 == 0)
+testprime a b =  if not (mod a b == 0) then testprime a (b-1) else False
+
+prime :: Integer -> Bool
+prime a = (testprime a ( toInt (sqrt (fromIntegral a))))
+
+testPrimesToNum ::(Num b) => [b] -> Integer -> Integer -> [b]
+testPrimesToNum a _ _ = testPrimesToNum a [] 0
+testPrimesToNum a b i = if a == i then b else if prime i then b ++ i && i == next i else  testPrimesToNum a b i
